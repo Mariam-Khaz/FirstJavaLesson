370caf77138014d82ce54819b70944eb263fed0e
Mariam-Khaz <mariam-khazroun@hotmail.com>
2024-10-16 00:50:22 +0200

    not first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// let x;
+// x = 125;
+
+// console.log(x);
+
+// let firstName = "Mariam";
+// let age = "29";
+// let favoriteColor = "Blue";
+
+// console.log(`Your firstName is ${firstName}`);
+// console.log(`Your age is ${age}`);
+// console.log(`Your favorite color is ${favoriteColor}`);
+
+
+
+// let online = false;
+// let forsale = true;
+// let isStudent = true;
+
+// condole.log(`Is this person online? ${online}`);
+// console.log(`Is this car for sale? ${forsale}`);
+// console.log(`Enrolled: ${isStudent}`);
+
+
+// let students = 30;
+
+// student = students = 30 + 1;
+// students = students - 1;
+// student = students x 2;
+// students = students / 2;
+// student = students % 2;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students++;
+// students--;
+
+// console.log(`My husbands age is ${age += 1}`);
+// console.log(`My husbands age is ${age ++}`);
+
+// let loggedInUser;
+// loggedInUser = "Mariam Khaz";
+
+0 / 0;  //NaN
+1 + NuN; //NaN
+
